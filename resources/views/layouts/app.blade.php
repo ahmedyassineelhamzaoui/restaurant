202e96273846f9564f53c15ee9e2c84d04cbeb1a
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-02-13 13:49:53 +0100

    deleting old files

--- resources/views/layouts/app.blade.php
+++
@@ -1,7 +0,0 @@
-@include('layouts/head')
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-            <main>
-                @include('layouts.sidebar')
-                
