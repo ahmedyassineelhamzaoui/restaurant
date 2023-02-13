ab7072c8215312b7d1483a878ec87e41fe50cc83
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-02-13 13:57:06 +0100

    add the content of CategoryFromrequest

--- app/Http/Requests/CategoryFromRequest.php
+++ app/Http/Requests/CategoryFromRequest.php
@@ -13,7 +13,7 @@ class CategoryFromRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class CategoryFromRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name_category'=>[
+                'required',
+                'max:50'
+            ]
         ];
     }
 }
