cf78c79f200e42df8561ccb6992eb43a9523dca6
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-02-13 14:22:50 +0100

    add neccessary routing inside web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,3 +30,27 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+    
+Route::controller(App\Http\Controllers\platController::class)->group(function () {
+    Route::get('/welcome', 'display');
+    Route::get('/', 'display');
+    Route::get('/dashboard','index');
+    Route::post('/dashboard', 'store')->name('dashboard');
+    Route::delete('/dashboard/{id}', 'destroy')->name('dashboard.destroy');
+    Route::get('/editPlat/{id}','edit');
+    Route::put('/dashboard/{id}','update')->name('dashboard.update');
+});
+
+
+Route::get('/category',function(){ return view('category');})->middleware(['auth','verified'])->name('category');
+
+Route::controller(App\Http\Controllers\categoryController::class)->group(function (){
+    //  Route::get('/dashboard','display');
+     Route::get('/category','index');
+     Route::post('/category','store')->name('category');
+     Route::delete('/category/{id}','destroy')->name('category.destroy');
+     Route::put('/category','update')->name('category.update');
+});
